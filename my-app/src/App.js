c82faf776790a56e2e4d1192f366fcbfbf5f57e0
unknown <ttddcc119@naver.com>
2024-02-21 23:15:49 +0900

    useState를 이용한 컴포넌트 변경

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,111 @@
 import { useState } from 'react';
 
-export default function App() {
-  const [isEditing, setIsEditing] = useState(false);
-  const [firstName, setFirstName] = useState('Jane');
-  const [lastName, setLastName] = useState('Jacobs');
+let nextId = 0;
 
-  function handleEdit(e) {
-    e.preventDefault();
-    setIsEditing(!isEditing);
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState('');
+
+  console.log('렌더링 됐습니다 !');
+
+  function handleType(e) {
+    setText(e.target.value);
+  }
+
+  function handleAdd() {
+    setTasks([...tasks, { id: nextId++, content: text, isEdit: false }]);
+  }
+
+  function handleSave(id, newContent) {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id)
+          return { id: id, content: newContent, isEdit: false };
+        return task;
+      }),
+    );
+  }
+
+  function handleEdit(targetId) {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === targetId) return { ...task, isEdit: true };
+        return task;
+      }),
+    );
   }
 
   return (
-    <form>
-      <label>
-        first Name :{' '}
-        {isEditing ? (
-          <input
-            placeholder={firstName}
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
-          />
-        ) : (
-          <b>{firstName}</b>
-        )}
-      </label>
-      <label>
-        lastName :{' '}
-        {isEditing ? (
-          <input
-            placeholder={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        ) : (
-          <b>{lastName}</b>
-        )}
-      </label>
-      <button type='submit' onClick={handleEdit}>
-        {isEditing ? 'Save Profile' : 'Edit Profile'}
-      </button>
-      <p>
-        <i>Hello, {firstName + ' ' + lastName}!</i>
-      </p>
-    </form>
+    <>
+      <h1>To Do List</h1>
+      <Input onChange={handleType} />
+      <Button onClick={handleAdd}>추가</Button>
+      <TodoList tasks={tasks} onSave={handleSave} onEdit={handleEdit} />
+    </>
+  );
+}
+
+function Input({ onChange }) {
+  return (
+    <input type='text' placeholder='할 일을 입력해주세요' onChange={onChange} />
+  );
+}
+
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
+}
+
+function TodoList({ tasks, onSave, onEdit }) {
+  if (!tasks) return;
+
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return (
+          <li key={task.id}>
+            {task.isEdit ? (
+              <TodoInput key={task.id} task={task} onSave={onSave} />
+            ) : (
+              <TodoText key={task.id} task={task} onEdit={onEdit} />
+            )}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function TodoInput({ task, onSave }) {
+  const [localText, setLocalText] = useState(task.content);
+  return (
+    <div>
+      <input
+        type='text'
+        onChange={(e) => setLocalText(e.target.value)}
+        value={localText}
+      />
+      <Button
+        onClick={() => {
+          onSave(task.id, localText);
+        }}
+      >
+        Save
+      </Button>
+    </div>
+  );
+}
+
+function TodoText({ task, onEdit }) {
+  return (
+    <>
+      <p>{task.content}</p>
+      <Button
+        onClick={() => {
+          onEdit(task.id);
+        }}
+      >
+        Edit
+      </Button>
+    </>
   );
 }
