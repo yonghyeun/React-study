c0ddaf157f5a799a6f3ab6dc8efddb7ed22d0856
unknown <ttddcc119@naver.com>
2024-02-23 18:01:57 +0900

    Component lifecycle

---
+++ 4.Sharing-State-Between-Components/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import './App.css';
+
+export default function App() {
+  const [counters, setCounters] = useState([
+    {
+      id: 0,
+      body: <Counter color='#038C7F' />,
+    },
+    {
+      id: 1,
+      body: <Counter color='#ADD9D1' />,
+    },
+    {
+      id: 2,
+      body: <Counter color='#D95252' />,
+    },
+  ]);
+
+  function handleOnclick() {
+    const copied = [...counters];
+    for (let i = copied.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copied[i], copied[j]] = [copied[j], copied[i]];
+    }
+    setCounters(copied);
+  }
+
+  return (
+    <>
+      <div className='wrapper'>
+        {counters.map((component) => (
+          <div key={component.id}>{component.body}</div>
+        ))}
+        <button onClick={handleOnclick}>Shuffle</button>
+      </div>
+    </>
+  );
+}
+
+function Counter({ color }) {
+  const [num, setNum] = useState(0);
+
+  return (
+    <div className='container' style={{ backgroundColor: color }}>
+      <p>{num}</p>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Increase Number
+      </button>
+    </div>
+  );
+}
