8f76aeee51fd4ee49909cbc396d1a22fa91d1369
unknown <ttddcc119@naver.com>
2024-02-28 18:13:11 +0900

    useEffect study

---
+++ 10.Synchronize-With-Effects/src/App.js
@@ -0,0 +1,16 @@
+useEffect(() => {
+  let ignore = false;
+
+  async function startFetching() {
+    const json = await fetchTodos(userId);
+    if (!ignore) {
+      setTodos(json);
+    }
+  }
+
+  startFetching();
+
+  return () => {
+    ignore = true;
+  };
+}, [userId]);
